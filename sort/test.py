53430b743801b6372767c3ad8364446965f146b2
Vincent Meng <suanmeiguo@gmail.com>
2016-05-02 23:00:10 -0700

    Add comments to each test function

--- sort/test.py
+++ sort/test.py
@@ -18,30 +18,44 @@ from nose.tools import assert_raises
 class TestSortingAlgorithm(object):
 
     def empty_input(self, fn):
+        """Test function can handle empty input
+        """
         print fn
         assert(fn([]) == [])
 
     def single_element(self, fn):
+        """Test function returns correct result for single element input
+        """
         ls = [5]
         assert(fn(ls) == ls)
 
     def sort_sorted(self, fn):
+        """Test sorting on a sorted list
+        """
         ls = [-1, 0, 1, 2, 3]
         assert(fn(ls) == ls)
 
     def random(self, fn):
+        """Test sorting result is correct
+        """
         ls = [5, 2, 6, 8, 0, 4, -2, -1]
         assert(fn(ls) == sorted(ls))
 
     def tuple_compatiable(self, fn):
+        """Test function handles tuple input
+        """
         tp = (0, 4, -3, 6, 20, 40.0)
         assert_raises(TypeError, fn, tp)
 
     def string_compatiable(self, fn):
+        """Test function handles string input
+        """
         s = r"082389hqwehfy\aoweFE$GH%TW#%TSDGSf"
         assert_raises(TypeError, fn, s)
 
     def test_all(self):
+        """Run all tests on each sorting algorithm
+        """
         for fn in (bubble_sort.sort, insertion_sort.sort):
             yield self.empty_input, fn
             yield self.single_element, fn
